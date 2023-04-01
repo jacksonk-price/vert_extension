1351fd84cba35f7df9aa10618e7eafc392f19651
Jackson Price <price.jacksonk@gmail.com>
2023-04-01 10:27:17 -0500

    init commit

---
+++ content.js
@@ -0,0 +1,4 @@
+console.log('hello test1 from seank');
+document.addEventListener("DOMContentLoaded", function() {
+    console.log('hello test1 from seank');
+});
---
+++ scripts/content.js
@@ -0,0 +1,5 @@
+console.log('hello test1 from seank');
+document.addEventListener("DOMContentLoaded", function() {
+    console.log('hello test1 from seank');
+});
+  
